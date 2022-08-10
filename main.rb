84b86bc80a14093b7ba815fdb42af6f4111a0404
imcousin <imcousin@users.noreply.github.com>
2022-08-10 07:34:00 +0000

    add show board

--- main.rb
+++ main.rb
@@ -1,6 +1,32 @@
-# Requirements
+# Odin Project Tic Tac Toe Requirements
 # Get Player1,2 Input
 # Show Board
 # Count Score
 # Calculate Score
 # Reset Board after win
+
+class Game
+  def initialize
+    get_players_name
+    show_board
+  end
+
+  def get_players_name
+    puts "What is player1's pretty name?"
+    player1 = gets
+    puts "What is player2's hola name?"
+    player2 = gets
+    puts 'Lets get ready to rumble!'
+  end
+
+  def show_board
+    puts "- | - | -"
+    puts "- | - | -"
+    puts "- | - | -"
+  end
+
+  def player_move
+  end
+end
+
+Game.new
